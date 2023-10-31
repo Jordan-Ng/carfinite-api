4d5c1c983a02b5f9b09f762a086a0f50f6fd6c1b
William <wsutanto@csu.fullerton.edu>
2023-10-31 14:13:10 -0700

    Implemented signout API and seeding database -updated seed.sql with new users and car_listing mock data -updated controller.js to implement handleSignout function -updated app.js to enable handleSignout function

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
-const express = require('express')
-const dotenv = require("dotenv")
-const authMiddleware = require("./middlewares/auth.js")
+const express = require("express");
+const dotenv = require("dotenv");
+const authMiddleware = require("./middlewares/auth.js");
 
-const app = express()
-const controller = require("./controller.js")
+const app = express();
+const controller = require("./controller.js");
 
-dotenv.config()
+dotenv.config();
 
+app.get("/", (req, res) => {
+  let routes = app._router.stack.map((r) => r.route?.path);
+  routes = routes.filter((route) => route && route != "/");
+  res.send({ routes: routes });
+});
 
-app.get('/', (req, res) => {
-    let routes = app._router.stack.map(r => r.route?.path)
-    routes = routes.filter(route => route && route != "/")
-    res.send({routes : routes})
-})
+app.use(express.json());
 
-app.use(express.json())
+app.post("/register", controller.handleRegister);
 
-app.post('/register', controller.handleRegister)
-
-app.post("/login", controller.handleLogin)
-
-
+app.post("/login", controller.handleLogin);
 
 // authentication controlled endpoints
-app.use(authMiddleware.authenticateToken)
+app.use(authMiddleware.authenticateToken);
 
-app.get('/listings', controller.handleGetAllListings)
-
-
-
-
-
+app.get("/listings", controller.handleGetAllListings);
 
 // binding app socket to port
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`App bound and running on port ${process.env.SERVER_PORT}`)
-})
+  console.log(`App bound and running on port ${process.env.SERVER_PORT}`);
+});
+
+//signout
+app.delete("/signout", controller.handleSignout);
